1066d9dd3857522ca5a83a497542a3f6b046e3c6
GabrielMarrogn <gabrielosantos2012@hotmail.com>
2024-01-19 10:07:16 -0300

    second

--- src/App.java
+++ src/App.java
@@ -16,64 +16,75 @@ public class App {
             String jogo = br.readLine();
             String[] dados = jogo.split(" ");
 
+            //empate
             if(dados[0].equals(dados[1])){
                 System.out.println("Caso #" +i +": De novo!");
             }
 
+            //a tesoura corta o papel
             if(dados[0].equals(tesoura) && dados[1].equals(papel)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(papel) && dados[1].equals(tesoura)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
 
+            //o papel embrulha a pedra
             if(dados[0].equals(papel) && dados[1].equals(pedra)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(pedra) && dados[1].equals(papel)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
             
+            //pedra esmaga o lagarto
             if(dados[0].equals(pedra) && dados[1].equals(lagarto)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(lagarto) && dados[1].equals(pedra)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
 
+            //lagarto envenena Spock
             if(dados[0].equals(lagarto) && dados[1].equals(spock)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(spock) && dados[1].equals(lagarto)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
 
+            //Spock destroi a tesoura
             if(dados[0].equals(spock) && dados[1].equals(tesoura)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(tesoura) && dados[1].equals(spock)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
 
+            //tesouta decapita o lagarto
             if(dados[0].equals(tesoura) && dados[1].equals(lagarto)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(lagarto) && dados[1].equals(tesoura)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
 
-            if(dados[1].equals(lagarto) && dados[1].equals(papel)){
+            //lagarto come o papel
+            if(dados[0].equals(lagarto) && dados[1].equals(papel)){
                 System.out.println("Caso #" +i +": Bazinga!");
-            }else if(dados[1].equals(papel) && dados[1].equals(lagarto)){
+            }else if(dados[0].equals(papel) && dados[1].equals(lagarto)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
 
+            //papel contesta Spock
             if(dados[0].equals(papel) && dados[1].equals(spock)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(spock) && dados[1].equals(papel)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
 
+            //Spock destoi a pedra
             if(dados[0].equals(spock) && dados[1].equals(pedra)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(pedra) && dados[1].equals(spock)){
                 System.out.println("Caso #" +i +": Raj trapaceou!");
             }
 
+            //Pedra quebre a tesoura
             if(dados[0].equals(pedra) && dados[1].equals(tesoura)){
                 System.out.println("Caso #" +i +": Bazinga!");
             }else if(dados[0].equals(tesoura) && dados[1].equals(pedra)){
